1359ab290a74d860e36fa8f85938367b571b329e
Epic-R-R <epicrr001@gmail.com>
2021-05-03 03:18:04 +0430

    details function

--- main.py
+++ main.py
@@ -9,13 +9,24 @@ try:
 except ImportError:
     print(f'{Fore.RED}Some modules are not installed! Installing them with this command.\n{Fore.GREEN}pip install -r requirement.txt')
 
+def details(link):
+    source_code = requests.get("https://www10.gogoanime.io"+link)
+    content = source_code.content
+    soup = BeautifulSoup(content,'html.parser')
+    container_soup = soup.find('div', {'class':'anime_info_body_bg'})
+    print("\nName of the Anime : ", container_soup.find('h1').getText(),"\n") 
+    titles_detail = container_soup.find_all('p',{'class':'type'})
+    for elem in titles_detail:
+        print(elem.getText())
+        print("\n")
+        
 def get_details(soup):
     raw_soup = soup.find_all('div', {"class":'img'})
     for item in raw_soup:
         temp_soup = item.find('a')
         title.append(temp_soup['title'])
         link.append(temp_soup['href'])
-        
+
 def links(title,link):
     for i in range(len(title)):
         print("%d. %s : https://www10.gogoanime.io%s\n" % (i+1,title[i],link[i]))
